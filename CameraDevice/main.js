c34e72c571f74bfb55ac538e6d590882b53572b2
vzakharchenko <vzakharchenko@modeln.com>
2020-07-30 14:29:47 +0300

    initial commit

---
+++ CameraDevice/main.js
@@ -0,0 +1,112 @@
+var serverInfo = {
+    ip: '0.0.0.0',
+    port:'3004',
+};
+
+
+
+var next = () =>{
+    var xhr = new XMLHttpRequest();
+    const url = 'http://'+serverInfo.ip+':'+serverInfo.port+'/next?width='+window.innerWidth+'&height='+window.innerHeight;
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {        // ready state event, will be executed once the server send back the data
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+                location.reload();
+            } else {
+            	console,error('There was a problem with the request.');
+            }
+        }
+    };
+    xhr.onerror = function (e) {
+      console.error(xhr.statusText);
+    };
+    xhr.send(null);
+}
+var prev = () =>{
+    var xhr = new XMLHttpRequest();
+    const url = 'http://'+serverInfo.ip+':'+serverInfo.port+'/prev?width='+window.innerWidth+'&height='+window.innerHeight
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {        // ready state event, will be executed once the server send back the data
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+                location.reload();
+            } else {
+            	console,error('There was a problem with the request.');
+            }
+        }
+    };
+    xhr.onerror = function (e) {
+      console.error(xhr.statusText);
+    };
+    xhr.send(null);
+}
+
+var reload = () =>{
+    var xhr = new XMLHttpRequest();
+    const url = 'http://'+serverInfo.ip+':'+serverInfo.port+'/reload?width='+window.innerWidth+'&height='+window.innerHeight
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {        // ready state event, will be executed once the server send back the data
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+                location.reload();
+            } else {
+                console,error('There was a problem with the request.');
+            }
+        }
+    };
+    xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+    };
+    xhr.send(null);
+}
+//
+// channelList()
+
+//Initialize function
+var init = function () {
+    // TODO:: Do your initialization job
+    console.log('init() called');
+
+    document.addEventListener('visibilitychange', function () {
+        if (document.hidden) {
+            // Something you want to do when hide or exit.
+        } else {
+            // Something you want to do when resume.
+        }
+    });
+
+    // add eventListener for keydown
+    document.addEventListener('keydown', function (e) {
+        switch (e.keyCode) {
+            case 37: //LEFT arrow
+                prev();
+                break;
+            case 38: //UP arrow
+                next();
+                break;
+            case 39: //RIGHT arrow
+                next();
+                break;
+            case 40: //DOWN arrow
+                prev();
+                break;
+            case 13: //OK button
+                reload()
+                break;
+            case 10009: //RETURN button
+                tizen.application.getCurrentApplication().exit();
+                break;
+            default:
+                console.log('Key code : ' + e.keyCode);
+                break;
+        }
+    });
+    document.body.focus();
+};
+// window.onload can work without <body onload="">
+window.onload = init;
+
---
+++ server.js
@@ -0,0 +1,157 @@
+const Stream = require('node-rtsp-stream');
+const {CronJob} = require('cron');
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.static(__dirname + "/CameraDevice"));
+
+
+console.log = () => {
+};
+console.error = () => {
+};
+console.debug = () => {
+};
+
+function readChannel() {
+    let channelJson = {channels: []};
+    const defaultChannelFile = './config/channels.json';
+    if (fs.existsSync(defaultChannelFile)) {
+        const defaultChannelJson = JSON.parse(fs.readFileSync(defaultChannelFile, 'UTF-8'));
+        channelJson = defaultChannelJson;
+    }
+    const ovverideChannelFile = './config/userChannels.json';
+    if (fs.existsSync(ovverideChannelFile)) {
+        const overrideChannel = JSON.parse(fs.readFileSync(ovverideChannelFile, 'UTF-8'));
+        channelJson = overrideChannel;
+    }
+    const channelsFile = '/opt/config/channels.json';
+    if (fs.existsSync(channelsFile)) {
+        const overrideChannel = JSON.parse(fs.readFileSync(channelsFile, 'UTF-8'));
+        channelJson = overrideChannel;
+    }
+
+    return channelJson.channels;
+};
+
+function readCurrentChannel() {
+    const currentChannelFile = '.currentChannel';
+    if (fs.existsSync(currentChannelFile)) {
+        const curJson = JSON.parse(fs.readFileSync(currentChannelFile, 'UTF-8'));
+        currentChannel = Number(curJson.currentChannel);
+        width = Number(curJson.width);
+        height = Number(curJson.height);
+    } else {
+        currentChannel = 0;
+        width = 1920;
+        width = 1080;
+    }
+}
+
+function saveCurrentChannel() {
+    const currentChannelFile = '.currentChannel';
+    fs.writeFileSync(currentChannelFile, JSON.stringify({currentChannel, width, height}));
+}
+
+const channels = readChannel();
+
+let currentChannel = 0;
+let width = 1920;
+let height = 1080;
+
+readCurrentChannel();
+
+let stream = null;
+
+function recreateStream() {
+    if (stream) {
+        stream.mpeg1Muxer.stream.kill();
+        stream.wsServer.close();
+    }
+    stream = new Stream({
+        name: currentChannel,
+        streamUrl: channels[currentChannel].streamUrl,
+        wsPort: 9999,
+        ffmpegOptions: { // options ffmpeg flags
+            '-vf': `scale=${width}:${height}`,
+            '-nostats': '',
+            '-loglevel': 'quiet',
+            //'-stats': '', // an option with no neccessary value uses a blank string
+            '-r': 30,
+        }
+    });
+}
+
+recreateStream();
+app.get('/next', (req, res) => {
+    var width0 = req.query.width;
+    if (width0) {
+        width = width0;
+    }
+    var height0 = req.query.height;
+    if (height0) {
+        height = height0;
+    }
+    const nextChannel = currentChannel + 1;
+    if (channels[nextChannel]) {
+        currentChannel = nextChannel;
+    } else {
+        currentChannel = 0;
+    }
+    saveCurrentChannel();
+    recreateStream();
+    return res.send('OK');
+});
+
+app.get('/prev', (req, res) => {
+    const nextChannel = currentChannel - 1;
+    var width0 = req.query.width;
+    if (width0) {
+        width = width0;
+    }
+    var height0 = req.query.height;
+    if (height0) {
+        height = height0;
+    }
+    if (channels[nextChannel]) {
+        currentChannel = nextChannel;
+    } else {
+        currentChannel = channels.length;
+    }
+    recreateStream();
+    saveCurrentChannel();
+    return res.send('OK');
+});
+
+app.get('/reload', (req, res) => {
+    var width0 = req.query.width;
+    if (width0) {
+        width = width0;
+    }
+    var height0 = req.query.height;
+    if (height0) {
+        height = height0;
+    }
+    recreateStream();
+    return res.send('OK');
+});
+
+function installCrons() {
+    const cronJob = new CronJob("0 */2 * * * *", (() => {
+        if (stream.mpeg1Muxer.stream.exitCode > 0 ||
+        !stream.mpeg1Muxer.stream.pid > 0 ||
+        !stream.mpeg1Muxer.inputStreamStarted ||
+        stream.mpeg1Muxer.stream.killed,
+        stream.mpeg1Muxer.stream._closesGot > 0) {
+            recreateStream();
+        }
+    }), null, true, 'America/Los_Angeles');
+    console.debug('System TZ next 5: ', cronJob.nextDates(5));
+}
+
+installCrons();
+
+
+app.listen(3004);

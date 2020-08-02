d62c69de1c58ce321675f501fb22ed12e4742090
vzakharchenko <vzakharchenko@modeln.com>
2020-08-02 23:38:34 +0300

    increase limit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const {
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 const corsOptions = {
     origin(o, callback) {
         callback(null, true);

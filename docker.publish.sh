b3196c6a6a97b186372489f695bee9214aa5b495
vzakharchenko <vzakharchenko@modeln.com>
2021-01-17 00:41:29 +0200

    update version

--- docker.publish.sh
+++ docker.publish.sh
@@ -1,6 +1,6 @@
 docker build -t rtsp-samsung-tv .
-docker tag  rtsp-samsung-tv vassio/rtsp-samsung-tv:1.0.9
-docker push vassio/rtsp-samsung-tv:1.0.9
+docker tag  rtsp-samsung-tv vassio/rtsp-samsung-tv:1.1.18
+docker push vassio/rtsp-samsung-tv:1.1.18
 
 docker tag  rtsp-samsung-tv vassio/rtsp-samsung-tv:latest
 docker push vassio/rtsp-samsung-tv:latest
